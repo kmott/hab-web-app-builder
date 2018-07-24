db2db472ad257fc1b5ca6ce464d6ee6561e60d13
Kyle Mott <kyle@klmh.co>
2018-07-24 07:08:00 -0700

    Use my ImageMagick package, rename package...

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,5 +1,5 @@
-pkg_name=habitatize
+pkg_name=hab-web-app-builder
 pkg_origin=kmott-test
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_version="0.4.0"
+pkg_version="0.2.0"
 pkg_deps=( kmott-test/imagemagick )
