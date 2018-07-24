4809a6469f5ee5079a6e2053d368ebb7e01cae0d
Kyle Mott <kyle@klmh.co>
2018-07-24 06:59:53 -0700

    Use core ImageMagick package...

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=habitatize
 pkg_origin=kmott-test
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_version="0.3.0"
-pkg_deps=( core/imagemagick )
+pkg_version="0.4.0"
+pkg_deps=( kmott-test/imagemagick )
